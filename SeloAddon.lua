1656871c2d582a8de481b4903bb58b9f6ff5e39c
daniel-w-f <daniel_wilms@gmx.de>
2022-11-15 15:16:12 +0100

    Experiments with "commands" after "slash command"

--- SeloAddon.lua
+++ SeloAddon.lua
@@ -3,6 +3,20 @@ SeloAddon = {}
 
 function SeloAddon.SlashCmdHandler(msg, editbox)
     print("Hello, WoW2!")
+    -- pattern matching that skips leading whitespace and whitespace between cmd and args
+    -- any whitespace at end of args is retained
+    local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)")
+        
+    if cmd == "add" and args ~= "" then
+        print("adding " .. args)
+        -- Handle adding of the contents of rest... to something.
+    elseif cmd == "remove" and args ~= "" then
+        print("removing " .. args)
+        -- Handle removing of the contents of rest... to something.   
+    else
+        -- If not handled above, display some sort of help message
+        print("Syntax: /hellow (add|remove) someIdentifier");
+    end
 end
 SLASH_ADDONSELO1 = "/as";
 SlashCmdList["ADDONSELO"] = SeloAddon.SlashCmdHandler;
