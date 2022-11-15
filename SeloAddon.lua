74893ad7d336a6c093c8e97ee310096fe15bbc56
daniel-w-f <daniel_wilms@gmx.de>
2022-11-15 14:45:01 +0100

    Initial playaround stuff

---
+++ SeloAddon.lua
@@ -0,0 +1,34 @@
+local SeloAddon = ...
+SeloAddon = {}
+
+function SeloAddon.SlashCmdHandler(msg, editbox)
+    print("Hello, WoW2!")
+end
+SLASH_ADDONSELO1 = "/as";
+SlashCmdList["ADDONSELO"] = SeloAddon.SlashCmdHandler;
+
+SLASH_SELOADDON1 = "/sa";
+SlashCmdList["SELOADDON"] = function(msg, _)
+    print("Hello, WoW!")
+end
+
+function SeloAddon_OnLoad()
+    -- this:RegisterEvent("VARIABLES_LOADED")
+
+    SLASH_TEST1 = "/test1"
+    SLASH_TEST2 = "/addontest1"
+    SlashCmdList["TEST"] = function(msg)
+        print("Hello World!")
+    end 
+end
+
+SeloAddon_SlashCommand = function (msg, _)
+    print("Hello, WoW2!")
+end
+
+-- 1. Pick HELLOWORLD as the unique identifier.
+-- 2. Pick /hiw and /hellow as slash commands (/hi and /hello are actual emotes)
+SLASH_HELLOWORLD1, SLASH_HELLOWORLD2 = '/hiw', '/hellow'; -- 3.
+function SlashCmdList.HELLOWORLD(msg, editbox) -- 4.
+    print("Hello, World!");
+end
